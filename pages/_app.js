2e461583de0456ebb24069585cdf07271d523704
German Kosach <fuerstein@yandex.ru>
2023-08-29 11:54:00 +0300

    production

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,10 @@
 import Head from 'next/head';
-
 import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }) => (
     <>
         <Head>
-            <title>Metaversus</title>
+            <title>BOBO X lands</title>
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="icon" href="/favicon.ico" />
             <link rel="preconnect" href="https://stijndv.com" />
